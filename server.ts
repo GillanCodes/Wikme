8ea32c9a83ea84767cca6dc7f5f233bd7df9a7b4
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-04-28 13:40:12 +0200

    Added Cors - Body parser and Cookie Parser

--- server.ts
+++ server.ts
@@ -1,8 +1,36 @@
 import * as express from "express";
 
+import bodyParser = require("body-parser");
+import cookieParser = require("cookie-parser");
+import cors = require('cors');
+import { Callback } from "mongoose";
+
 let app:express.Application = express();
 
 require('./config/database');
+
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit: 50000}));
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(cookieParser());
+
+let whiteList = [undefined, 'http://localhost:3000', 'localhost:3000'];
+const corsOptions:Object = {
+    origin : function (origin:string, cb:Callback) {
+        if (whiteList.indexOf(origin) !== -1)
+        {
+            cb(null, true);
+        } else {
+            cb(new Error('Not Allowed by CORS'), true);
+        }
+    },
+    'credentials' : true,
+    'allowHeaders' : ['sessionId', 'Content-Type', 'Authorization'],
+    'exposedHeaders' : ['sessionId'],
+    'methods' : 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false,
+    'optionsSuccessStatus': 200
+}
+app.use(cors(corsOptions));
 
 app.listen(process.env.PORT, () => {
     console.log(`Wikme listening to : PORT ${process.env.PORT}`)
