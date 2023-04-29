e650809aa4d1dde8e0776dc23c5de90326338412
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-04-29 13:40:35 +0200

    Create function to manage blocks

--- client/src/components/Editor/BlockModal.jsx
+++ client/src/components/Editor/BlockModal.jsx
@@ -1,88 +1,48 @@
 import React from 'react'
 
 export default function BlockModal({setModal}) {
-  return (
-    <div className='block-adder'>
 
-        <div className="block-adder-container">
-            <div className="block-adder-content">
-                <div className="head">
-                    <h1>Add New Block</h1>
-                </div>
-                <div className="body">
+    const addBlock = (type) => {
+        setModal(0);
+        
+    }
 
-                   <div className="items">
-                        <div className="item">
-                            <h2 className='title'>Text</h2>
-                            <p>Just a text Box</p>
-                        </div>
+    return (
+        <div className='block-adder'>
 
-                        <div className="item">
-                            <h2 className='title'>Caption</h2>
-                            <p>Text Box with a image on side</p>
-                        </div>
+            <div className="block-adder-container">
+                <div className="block-adder-content">
+                    <div className="head">
+                        <h1>Add New Block</h1>
+                    </div>
+                    <div className="body">
 
-                        <div className="item">
-                            <h2 className='title'>Caption Right</h2>
-                            <p>Text Box with a image on Right side</p>
-                        </div>
+                    <div className="items">
+                            <div className="item" id='text-only' onClick={() => addBlock("text-only")}>
+                                <h2 className='title'>Text</h2>
+                                <p>Just a text Box</p>
+                            </div>
 
-                        <div className="item">
-                            <h2 className='title'>Text</h2>
-                            <p>Just a text Box</p>
-                        </div>
+                            <div className="item">
+                                <h2 className='title'>Caption</h2>
+                                <p>Text Box with a image on side</p>
+                            </div>
 
-                        <div className="item">
-                            <h2 className='title'>Caption</h2>
-                            <p>Text Box with a image on side</p>
-                        </div>
-
-                        <div className="item">
-                            <h2 className='title'>Caption Right</h2>
-                            <p>Text Box with a image on Right side</p>
+                            <div className="item">
+                                <h2 className='title'>Caption Right</h2>
+                                <p>Text Box with a image on Right side</p>
+                            </div>
                         </div> 
 
-
-                        <div className="item">
-                            <h2 className='title'>Text</h2>
-                            <p>Just a text Box</p>
-                        </div>
-
-                        <div className="item">
-                            <h2 className='title'>Caption</h2>
-                            <p>Text Box with a image on side</p>
-                        </div>
-
-                        <div className="item">
-                            <h2 className='title'>Caption Right</h2>
-                            <p>Text Box with a image on Right side</p>
-                        </div>
-
-                        <div className="item">
-                            <h2 className='title'>Text</h2>
-                            <p>Just a text Box</p>
-                        </div>
-
-                        <div className="item">
-                            <h2 className='title'>Caption</h2>
-                            <p>Text Box with a image on side</p>
-                        </div>
-
-                        <div className="item">
-                            <h2 className='title'>Caption Right</h2>
-                            <p>Text Box with a image on Right side</p>
-                        </div> 
-                    </div> 
+                    </div>
+                    <div className="footer">
+                        <button onClick={() => setModal(0)}>Close</button>
+                    </div>
 
                 </div>
-                <div className="footer">
-                    <button onClick={() => setModal(0)}>Close</button>
-                </div>
-
             </div>
+
+
         </div>
-
-
-    </div>
-  )
+    )
 }
---
+++ client/src/components/Editor/blocks.js
@@ -0,0 +1,11 @@
+function createBlock()
+{
+
+}
+
+function deleteBlock() 
+{
+
+}
+
+export {createBlock, deleteBlock};
