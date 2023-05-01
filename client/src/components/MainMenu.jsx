956b2ca6ac2c3d1473f84df46b636b8e72bae1e7
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-01 12:37:56 +0200

    Main menu / navBar added

---
+++ client/src/components/MainMenu.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import { isEmpty } from '../utils';
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+
+
+export default function MainMenu() {
+
+  const [load, setLoad] = useState(false);
+  const currentUser = useSelector(state => state.userReducer); 
+
+
+  useEffect(() => {
+    console.log(isEmpty(currentUser))
+    if(!isEmpty(currentUser))
+    {
+      setLoad(true);
+    }
+  }, [currentUser]);
+
+  const logoutHandle = () => {
+    axios({
+      method: "post",
+      withCredentials: true,
+      url: `${process.env.REACT_APP_API_URL}/auth/logout`
+    }).then(() => {
+      window.location = "/auth"
+    })
+  }
+
+  return (
+    <div className='main-menu navbar-container'>
+        <div className="content">
+            <div className="items">
+              {/* <div className="item has-dropdown">
+                <div className="dropdown-text">
+                  <p>Wiki</p>
+                </div>
+                <div className="dropdown">
+                  <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                  </ul>
+                </div>
+              </div> */}
+              <div className="item">
+                <NavLink to={"/"}>Home</NavLink>
+              </div>
+              {load ? (
+                <>
+                  <div className="item">
+                    <NavLink to={"/me"}>{currentUser.username}</NavLink>
+                  </div>
+                  <div className="item" onClick={() => logoutHandle()}>
+                    <p>Logout</p>
+                  </div>
+                </> 
+              ) : (
+                <div className="item">
+                  <NavLink to={"/auth"}>Login/Register</NavLink>
+                </div>
+              )} 
+            </div>
+        </div>
+    </div>
+  )
+}
--- client/src/components/Routes/index.jsx
+++ client/src/components/Routes/index.jsx
@@ -3,10 +3,12 @@ import Page from "../Page";
 import HomePage from "../HomePage";
 import Editor from "../Editor/Editor";
 import Auth from "../Auth/Auth";
+import MainMenu from "../MainMenu";
 
 export default function index() {
   return (
     <BrowserRouter>
+        <MainMenu />
         <Routes>
             <Route path="/" Component={HomePage} />
             <Route path="/auth" Component={Auth} />
