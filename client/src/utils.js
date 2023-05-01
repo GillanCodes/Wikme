8c30f17a2f9aadf73464120bb346695a819995df
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-01 12:36:57 +0200

    typo false result fix

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,8 +1,9 @@
 export const isEmpty = (value) => {
+    console.log(typeof value)
     return (
-        value === undefined,
-        value === null,
-        (typeof value === "object" && Object.keys(value).length === 0),
+        value === undefined ||
+        value === null ||
+        (typeof value === "object" && Object.keys(value).length === 0) ||
         (typeof value === "string" && value.trim().length === 0)
     );
 }
