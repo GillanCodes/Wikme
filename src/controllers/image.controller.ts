5430e7a4ea684485318db578f96d0966e865fded
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-04 11:05:03 +0200

    controller image added

---
+++ src/controllers/image.controller.ts
@@ -0,0 +1,9 @@
+import * as express from "express";
+
+export const getImages = (req: express.Request, res: express.Response) => {
+
+}
+
+export const postImage = (req: express.Request, res: express.Response) => {
+
+}
--- src/routes/image.routes.ts
+++ src/routes/image.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 let router:Router = Router();
 
-router.get('/');
-router.post('/');
+import { getImages, postImage } from "../controllers/image.controller";
+
+router.get('/', getImages);
+router.post('/', postImage);
 
 export default router;
