5bb2714cf475d5e9f587f0c450964f80c8f89e65
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-04 11:59:05 +0200

    added cdn to public path

--- server.ts
+++ server.ts
@@ -38,6 +38,8 @@ app.get('/api/jwtid', requireAuth, (req: express.Request, res: express.Response)
     res.status(200).send(res.locals.user.id);
 });
 
+app.use('/cdn', express.static('./cdn'));
+
 import authRoutes from './src/routes/auth.routes';
 import userRoutes from './src/routes/user.routes';
 import wikiRoutes from './src/routes/wiki.routes';
