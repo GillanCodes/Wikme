a3fec8e323b55a5ed23e97affdb8a3bb42600ca4
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-04 11:47:24 +0200

    added path for CDN

--- config/config.ts
+++ config/config.ts
@@ -7,19 +7,22 @@ interface ENV {
     PORT : number | undefined;
     JWT_TOKEN: string | undefined;
     DB_CONNECT_STRING: string | undefined;
+    CDN_PATH: string | undefined;
 };
 
 interface Config {
     PORT : number;
     JWT_TOKEN: string;
     DB_CONNECT_STRING: string;
+    CDN_PATH: string;
 }
 
 const getConfig = (): ENV => {
     return {
         PORT: process.env.PORT ? Number(process.env.PORT) : undefined,
         DB_CONNECT_STRING: process.env.DB_CONNECT_STRING,
-        JWT_TOKEN: process.env.JWT_TOKEN
+        JWT_TOKEN: process.env.JWT_TOKEN,
+        CDN_PATH: process.env.CDN_PATH
     };
 };
 
