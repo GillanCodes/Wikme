ceeea42c3a436ec28782b06da06e9c3913f6e5de
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-04 11:47:54 +0200

    upload image sys

--- src/controllers/image.controller.ts
+++ src/controllers/image.controller.ts
@@ -1,9 +1,27 @@
 import * as express from "express";
+import config from "../../config/config";
+
+import * as fs from "fs";
+import genUId from "../utils/UId";
 
 export const getImages = (req: express.Request, res: express.Response) => {
 
 }
 
-export const postImage = (req: express.Request, res: express.Response) => {
+export const postImage = async (req: any, res: express.Response) => {
+    try {
+        if (req.file.mimetype !==  "image/jpg" && req.file.mimetype !== "image/png" && req.file.mimetype !== "image/jpeg") throw Error('Invalide Type ! ');
+        if (req.file.size > 500000) throw Error('Max Size !');
+    } catch (error) {
+        console.log(error);
+    }
+
+    const fileName = `${genUId()}.jpg`;
+
+    console.log(req.file.buffer);
+
+    fs.writeFile(`${config.CDN_PATH}/${fileName}`, req.file.buffer, (err) => {
+        if (err) console.log(err);
+    });
 
 }
--- src/routes/image.routes.ts
+++ src/routes/image.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 let router:Router = Router();
 
+import multer = require("multer");
+const upload = multer();
+
 import { getImages, postImage } from "../controllers/image.controller";
 
 router.get('/', getImages);
-router.post('/', postImage);
+router.post('/', upload.single('picture'), postImage);
 
 export default router;
