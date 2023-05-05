3ff8ecb863b9def6cc5056ca2985cab5cf2cae08
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-05 21:18:59 +0200

    added creation wiki form

--- client/src/components/Wiki/Wikis.jsx
+++ client/src/components/Wiki/Wikis.jsx
@@ -8,6 +8,8 @@ export default function Wikis() {
   const wikis = useSelector((state) => state.wikiReducer);
   const [load, setLoad] = useState(false);
 
+  const [state, setState] = useState({name: "", description: ""})
+
   useEffect(() => {
     if (!isEmpty(wikis))
     { 
@@ -16,9 +18,31 @@ export default function Wikis() {
     }
   }, [wikis]);
 
+  const createWikiHandle = () => {
+    console.log(state);
+  }
+
   return (
     <div className='wikis-container container'>
       <div className="wikis">
+        <div className='article-module'>
+          <div className="article-module-container">
+              <div className="article-module-content">
+                  <div className="article-module-head">
+                      <p>New Wiki</p>
+                  </div>
+                  <div className="article-module-body">
+                    <div className="form">
+                      <input type="text" className="input" value={state.name} onChange={(e) => setState({...state, name: e.target.value})} />
+                      <input type="text" className="input" value={state.description} onChange={(e) => setState({...state, description: e.target.value})} />
+                    </div>
+                  </div>
+                  <div className="article-module-footer">
+                      <p className='button' onClick={() => createWikiHandle()}>Create</p> 
+                  </div>
+              </div>
+          </div>
+        </div>
         {load && (
         <>
           {/* <WikiModule wiki={wikis[0]} /> */}
