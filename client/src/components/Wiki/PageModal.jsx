dd3ce004bb0a5c267f5ad020b1314da3027ae8d9
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-06 12:09:50 +0200

    classes changed

--- client/src/components/Wiki/PageModal.jsx
+++ client/src/components/Wiki/PageModal.jsx
@@ -2,23 +2,28 @@ import React from 'react'
 
 export default function PageModal({setModal, pageModal}) {
   return (
-    <div className='block-adder'>
-
-            <div className="block-adder-container">
-                <div className="block-adder-content">
-                    <div className="head">
-                        <h2>{pageModal.page.name}</h2>
-                    </div>
-                    <div className="body">
-                        <div className="items">
-                            
-                        </div> 
-                    </div>
-                    <div className="footer">
-                        <button onClick={() => setModal({...pageModal, isOpen:false})}>Close</button>
-                    </div>
+    <div className='page-modal'>
+        <div className="page-modal-container">
+            <div className="page-modal-content">
+                <div className="head">
+                    <h2>{pageModal.page.name}</h2>
+                </div>
+                <div className="body">
+                    <div className="items">
+                        <div className="form">
+                            <input type="text" className="input" placeholder="Page's name" value={pageModal.page.name} />
+                            <button className="button is-success">Save</button>
+                        </div>
+                        <div className="form">
+                            <div className="button is-danger">Delete</div>
+                        </div>
+                    </div> 
+                </div>
+                <div className="footer">
+                    <button onClick={() => setModal({...pageModal, isOpen:false})}>Close</button>
                 </div>
             </div>
         </div>
+    </div>
   )
 }
