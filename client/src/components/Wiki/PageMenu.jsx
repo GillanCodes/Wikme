7f8413c0b6d8fad26bb704b8412db86f596b333d
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-06 12:02:03 +0200

    new page menu edited

--- client/src/components/Wiki/PageMenu.jsx
+++ client/src/components/Wiki/PageMenu.jsx
@@ -1,38 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from '../Modules/Modal';
+import PageModal from './PageModal';
 
-export default function PageMenu({pages, wiki, createPageHandle, setNewPage, newPage}) {
-  return (
-    <div className='page-menu'>
-        <div className="container">
-            <div className="content">
-                <div className="head">
-                    <h2 className='title'>{wiki.name}</h2>
-                </div>
-                <div className="body">
-                    <div className="items">
-                        {pages.map((item) => {
-                            return (
+export default function PageMenu({pages, wiki, createPageHandle, setNewPage, newPage, setPageKey, pageKey}) {
+
+    const [pageModal, setPageModal] = useState({
+        isOpen: false,
+        page: null
+    });
+
+    return (
+        <>
+            <div className='page-menu'>
+                <div className="container">
+                    <div className="content">
+                        <div className="head">
+                            <h2 className='title'>{wiki.name}</h2>
+                        </div>
+                        <div className="body">
+                            <div className="items">
+                                {pages.map((item, key) => {
+                                    return (
+                                        <div className={pageKey === key ? 'item active' : 'item'}>
+                                            <button className='button' onClick={() => setPageModal({...pageModal, isOpen:true, page:item})}> 
+                                                <i className="fa-solid fa-gear setting"></i>
+                                            </button>
+                                            <p onClick={() => setPageKey(key)}>{item.name}</p>
+                                        </div>
+                                    )
+                                })}
                                 <div className="item">
-                                    <button className='button'> 
-                                        <i className="fa-solid fa-gear setting"></i>
+                                    <button className="button is-success" onClick={() => createPageHandle()}>
+                                        <i class="fa-solid fa-plus"></i>
                                     </button>
-                                    <p>{item.name}</p>
+                                    <input type="text" className="input" placeholder="New Page's name" value={newPage} onChange={(e) => setNewPage(e.target.value)}/>
                                 </div>
-                            )
-                        })}
-                        <div className="item">
-                            <button className="button is-success" onClick={() => createPageHandle()}>
-                                <i class="fa-solid fa-plus"></i>
-                            </button>
-                            <input type="text" className="input" placeholder="New Page's name" value={newPage} onChange={(e) => setNewPage(e.target.value)}/>
+                            </div>
+                        </div>
+                        <div className="foot">
+
                         </div>
                     </div>
                 </div>
-                <div className="foot">
-
-                </div>
             </div>
-        </div>
-    </div>
-  )
+
+            {pageModal.isOpen && (
+                <Modal content={<PageModal setModal={setPageModal} pageModal={pageModal} />} />
+            )}
+        </>
+    )
 }
