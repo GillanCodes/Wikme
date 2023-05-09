b049536ae6b6742098c85b87b17a591b99e2c1cc
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-09 18:00:42 +0200

    block control menu ended

--- client/src/components/Wiki/BlocksMenu.jsx
+++ client/src/components/Wiki/BlocksMenu.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { blockDown, blockUp, deleteBlock } from '../Editor/blocks';
 
 export default function BlocksMenu({page}) {
+
+  const [selcBlock, setSelcBlock] = useState({key: null, current: null});
+
   return (
     <div className='blocks-menu'>
       <div className="container">
@@ -10,9 +14,27 @@ export default function BlocksMenu({page}) {
           </div>
           <div className="body" key="blocks-list-body">
             <div className="items">
-              <div className="item">
-               
-              </div>
+              {page.content.map((block, key) => {
+                return (
+                  <div className="item" onClick={() => {if (selcBlock.key === key) {setSelcBlock({...selcBlock, key: null}) } else { setSelcBlock({...selcBlock, key: key})} }}>
+                    {block.type === "images" ? (
+                      <p>#{key+1} {block.type} - {block.content.length} pictures</p>
+                    ) : (
+                      <p>#{key+1} {block.type} - {block.content.slice(0, 10)}...</p>
+                    )}
+
+                    {selcBlock.key === key && (
+                      <div className="controls">
+                        <button className="button is-info" onClick={() => blockUp(key, page._id)}><i class="fa-solid fa-arrow-up"></i></button>
+                        <button className="button is-danger" onClick={() => deleteBlock(block.UId, page._id)}><i className="fa-solid fa-trash"></i></button>
+                        <button className="button is-info" onClick={() => blockDown(key, page._id)}><i class="fa-solid fa-arrow-down"></i></button>
+                      </div>
+                    )}
+                    
+                  </div>
+                )
+              })}
+              
 
             </div>
           </div>
