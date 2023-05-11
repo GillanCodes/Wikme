cce1944c93fb2546b2f2e547e9aa408cc34a15bc
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-11 19:37:14 +0200

    fix error: empty page.content

--- client/src/components/Wiki/Viewer/Viewer.jsx
+++ client/src/components/Wiki/Viewer/Viewer.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import Block from '../Editor/Block'
+import { isEmpty } from '../../../utils'
 
 export default function Viewer({page}) {
   return (
-  <div className="page">
-            <div className='editor-container'>
-                <div className="head">
-                </div>
-                <div className='content'>
-                    <div className="editor">
-                        <div className="editor-content" id="editor">
-                            
-                                <>
-                                    {page.content.map((item, key, arr) => {
-                                        return (
-                                            <div className='box' key={key}>
-                                                <p className='block-nb'>#{key+1}</p>
-                                                <Block block={item} editable={false} />
-                                            </div>
-                                        )
-                                    })}
-                                </> 
-                        </div>
-                    </div>
-                </div>
+    <div className="page">
+      <div className='editor-container'>
+        <div className='content'>
+          <div className="editor">
+            <div className="editor-content" id="editor">
+              {!isEmpty(page) && (
+                <>
+                  {page.content.map((item, key, arr) => {
+                    return (
+                      <div className='box' key={key}>
+                        <p className='block-nb'>#{key+1}</p>
+                        <Block block={item} editable={false} />
+                      </div>
+                    )
+                  })}
+                  </> 
+                )}
+              </div>
             </div>
         </div>
+      </div>
+    </div>
   )
 }
