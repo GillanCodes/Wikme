73667f70070d0940b34190b69e7d618c003257eb
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-12 15:29:24 +0200

    Update Viewer.jsx

--- client/src/components/Wiki/Viewer/Viewer.jsx
+++ client/src/components/Wiki/Viewer/Viewer.jsx
@@ -9,32 +9,33 @@ export default function Viewer({page}) {
   const [modal, setModal] = useState(null);
 
   return (
-    <div className="page">
-      <div className='editor-container'>
-        <div className='content'>
-          <div className="editor">
-            <div className="editor-content" id="editor">
-              {!isEmpty(page) && (
-                <>
-                  {page.content.map((item, key, arr) => {
-                    return (
-                      <div className='box' key={key}>
-                        <p className='block-nb'>#{key+1}</p>
-                        <Block block={item} editable={false} setImageModal={setModal} />
-                      </div>
-                    )
-                  })}
-                  </> 
-                )}
+    <>
+      <div className="page">
+        <div className='editor-container'>
+          <div className='content'>
+            <div className="editor">
+              <div className="editor-content" id="editor">
+                {!isEmpty(page) && (
+                  <>
+                    {page.content.map((item, key) => {
+                      return (
+                        <div className='box' key={key}>
+                          <Block block={item} editable={false} setImageModal={setModal} />
+                        </div>
+                      )
+                    })}
+                    </> 
+                  )}
+                </div>
               </div>
-            </div>
+          </div>
         </div>
+
+        {!isEmpty(modal) && (
+          <Modal content={<ImageModal modal={modal} setModal={setModal} />} />
+        )}
+
       </div>
-
-      {!isEmpty(modal) && (
-        <Modal content={<ImageModal modal={modal} setModal={setModal} />} />
-      )}
-
-    </div>
+    </>
   )
 }
