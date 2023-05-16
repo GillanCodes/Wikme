cc42c1a0146e986adb2d6c258335cb162ca82301
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-16 14:17:21 +0200

    Client => register errors done

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { isEmpty } from '../../utils';
 
 export default function Register() {
 
   const [state, setState] = useState({email: "", username: "", password: ""});
+  const [errors, setErrors] = useState();
 
   const registerHandle = (event) => {
     event.preventDefault();
@@ -17,7 +19,12 @@ export default function Register() {
         username: state.username,
         password: state.password
       }
-    }).then(() => window.location = window.location).catch((err) => console.log(err));
+    }).then((res) => {
+      if (res.data.errors) 
+        setErrors(res.data.errors);
+      else
+        window.location = window.location;
+    })
 
   }
 
@@ -27,20 +34,29 @@ export default function Register() {
         <form className="form" onSubmit={(e) => registerHandle(e)}>
           <div className="field">
             <label className="label">Email</label>
+            {!isEmpty(errors) && !isEmpty(errors.email) && (
+              <p>{errors.email}</p>
+            )}
             <div className="control">
-              <input type="email" name="email" id="email" className="input is-info" onChange={(e) => setState({...state, email:e.target.value})}/>
+              <input type="email" name="email" id="email" className={!isEmpty(errors) && !isEmpty(errors.email) ? "input is-danger" : "input is-info"} onChange={(e) => setState({...state, email:e.target.value})}/>
             </div>
           </div>
           <div className="field">
             <label className="label">Username</label>
+            {!isEmpty(errors) && !isEmpty(errors.username) && (
+              <p>{errors.username}</p>
+            )}
             <div className="control">
-              <input type="text" name="username" id="username" className="input is-info" onChange={(e) => setState({...state, username:e.target.value})}/>
+              <input type="text" name="username" id="username" className={!isEmpty(errors) && !isEmpty(errors.username) ? "input is-danger" : "input is-info"} onChange={(e) => setState({...state, username:e.target.value})}/>
             </div>
           </div>
           <div className="field">
             <label className="label">Password</label>
+            {!isEmpty(errors) && !isEmpty(errors.password) && (
+              <p>{errors.password}</p>
+            )}
             <div className="control">
-              <input type="password" name="password" id="password" className="input is-info" onChange={(e) => setState({...state, password:e.target.value})}/>
+              <input type="password" name="password" id="password" className={!isEmpty(errors) && !isEmpty(errors.password) ? "input is-danger" : "input is-info"} onChange={(e) => setState({...state, password:e.target.value})}/>
             </div>
           </div>
           <div className="field">
