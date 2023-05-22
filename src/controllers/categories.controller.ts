2576ef49718ef92937c9410a362fa8efc66933ea
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-22 16:43:50 +0200

    Update categories.controller.ts

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -5,7 +5,7 @@ import categoryModel from "../../models/category.model";
 
 export const getCategories = async (req:Request, res:Response) => {
     try {
-        const { wikiId } = req.body;
+        const { wikiId } : { wikiId:string } = req.body;
         if (isEmpty(wikiId)) throw Error('empty_field : wikiId');
         if (!isValidObjectId(wikiId)) throw Error("invalid_format_ObjectId : wikiId");
         
@@ -19,9 +19,9 @@ export const getCategories = async (req:Request, res:Response) => {
 
 export const createCategories = (req:Request, res:Response) => {
     try {
-        const { wikiId, name } = req.body;
+        const { wikiId, name } : {wikiId:string, name:string} = req.body;
         if (isEmpty(wikiId)) throw Error("empty_field : wikiId");
-        if (isEmpty(name)) throw Error("empty_field : name");
+        if (isEmpty(name)) throw Error("empty_field : categories.name");
         if (!isValidObjectId(wikiId)) throw Error('invalid_format_ObjectId : wikiId');
 
         categoryModel.create({
@@ -40,9 +40,34 @@ export const createCategories = (req:Request, res:Response) => {
 };
 
 export const updateCategories = (req:Request, res:Response) => {
+    try {
+        const { catId } = req.params;
+        const { name, childs } : {name:string, childs:[string]} = req.body;
 
+        if (!isValidObjectId(catId)) throw Error('');
+        if (isEmpty(name)) throw Error('empty_field : categories.name');
+        if (isEmpty(childs)) throw Error('empty_field : categories.childs');
+
+        categoryModel.updateMany({
+            $set: {
+                name,
+                childs
+            }
+        }, {new:true, upsert: true}).then((data) => {
+            return res.status(201).send({data});
+        }).catch((err) => {
+            throw Error (err);
+        });
+    } catch (error) {
+        console.log(error);
+        res.send(403).send(error);
+    };
 };
 
 export const deleteCategories = (req:Request, res:Response) => {
-
+    try {
+        
+    } catch (error) {
+        
+    }
 };
